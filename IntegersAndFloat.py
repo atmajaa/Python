2fe78303d135f4932ab54e4fdad7efe18a1dcad2
Atmaja P <69838867+atmajaa@users.noreply.github.com>
2024-04-24 23:10:34 +0530

    Integers and Float

---
+++ IntegersAndFloat.py
@@ -0,0 +1,16 @@
+import random
+print(random.randrange(1,10))
+num = 1
+num *= 10
+print(num)
+#printing absolute values
+print(abs(-9))
+#Rounding upto nearest Integer value
+print(round(2.678787878,5))
+#Type of a variable
+print(type(num))
+x = 35e3
+print(type(x))
+#Conversion from int to float
+y = float(num)
+print(y)
